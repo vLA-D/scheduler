a00b12c0970216a7e8f65324c25f5705aa895d32
VladimirV <vladimir.vulovic@quantox.com>
2018-10-09 16:31:21 +0200

    Minor fix on forgotten password view.

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <div class="row justify-content-center mt-5">
+        <div class="col-md-8 mt-4">
             <div class="card">
                 <div class="card-header">{{ __('Reset Password') }}</div>
 
@@ -33,7 +33,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-outline-dark">
+                                <button type="submit" class="btn btn-dark">
                                     {{ __('Send Password Reset Link') }}
                                 </button>
                             </div>
