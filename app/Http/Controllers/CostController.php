fbcf438558197acfad8b98c9070fb4f6a05bc418
VladimirV <vladimir.vulovic@quantox.com>
2018-11-01 14:33:52 +0100

    Added ordering of costs.

--- app/Http/Controllers/CostController.php
+++ app/Http/Controllers/CostController.php
@@ -29,7 +29,7 @@ class CostController extends Controller
      */
     public function index()
     {
-        $costs = auth()->user()->costs()->with('seller')->paginate(5);
+        $costs = auth()->user()->costs()->with('seller')->latest()->paginate(5);
 
         return view('cost.index', ['costs' => $costs]);
     }
